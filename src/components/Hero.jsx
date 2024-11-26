d23214047fc813ab7c924afd9c7ceb783072c07f
Sahil <79301513+sahildolia@users.noreply.github.com>
2024-11-26 11:11:21 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import heroImage from "../assets/hero-main.jpg";
+import { Helmet } from "react-helmet";
 const Hero = () => {
   return (
     <>
+     <Helmet>
+        <title>Home - Jata Pushp</title>
+      </Helmet>
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
